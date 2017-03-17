70b428a1327b1342292a0ab31d0c5678436e9411
quy <dinhquyf8@gmail.com>
2017-03-17 09:00:37 +0700

    updated db config

--- config/database.php
+++ config/database.php
@@ -26,7 +26,19 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
+    
+    'pgsql' => [
+        'driver'   => 'pgsql',
+        'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+        'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+        'username' => parse_url(getenv("DATABASE_URL"))["user"],
+        'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+        'charset'  => 'utf8',
+        'prefix'   => '',
+        'schema'   => 'public',
+    ],
 
     /*
     |--------------------------------------------------------------------------
