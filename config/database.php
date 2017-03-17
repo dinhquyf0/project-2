4936c1b666f478a346ee14bbe3c57d2eee802aba
quy <dinhquyf8@gmail.com>
2017-03-17 10:48:41 +0700

    new updated db config

--- config/database.php
+++ config/database.php
@@ -26,19 +26,19 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-    // 'default' => 'pgsql',
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
     
-    // 'pgsql' => [
-    //     'driver'   => 'pgsql',
-    //     'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-    //     'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-    //     'username' => parse_url(getenv("DATABASE_URL"))["user"],
-    //     'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-    //     'charset'  => 'utf8',
-    //     'prefix'   => '',
-    //     'schema'   => 'public',
-    // ],
+    'pgsql' => [
+        'driver'   => 'pgsql',
+        'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+        'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+        'username' => parse_url(getenv("DATABASE_URL"))["user"],
+        'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+        'charset'  => 'utf8',
+        'prefix'   => '',
+        'schema'   => 'public',
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -78,10 +78,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
