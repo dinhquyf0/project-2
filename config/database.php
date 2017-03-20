15a32ee91bff2071de5f95a00aaba382f45b4c47
quy <dinhquyf8@gmail.com>
2017-03-20 19:43:38 +0700

    new fix migrate

--- config/database.php
+++ config/database.php
@@ -80,6 +80,7 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
