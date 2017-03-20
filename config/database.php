02ce23429f15dfd3263feaf103c703817f5d4e52
quy <dinhquyf8@gmail.com>
2017-03-20 20:48:46 +0700

    Add Heroku creds and update default route to hit the DB.

--- config/database.php
+++ config/database.php
@@ -1,6 +1,11 @@
 <?php
 // require('../vendor/autoload.php');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -27,19 +32,19 @@ return [
     |
     */
 
-    // 'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => 'pgsql',
+    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => 'pgsql',
     
-        'pgsql' => [
-        'driver'   => 'pgsql',
-        'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-        'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-        'username' => parse_url(getenv("DATABASE_URL"))["user"],
-        'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-        'charset'  => 'utf8',
-        'prefix'   => '',
-        'schema'   => 'public',
-    ],
+    //     'pgsql' => [
+    //     'driver'   => 'pgsql',
+    //     'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+    //     'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+    //     'username' => parse_url(getenv("DATABASE_URL"))["user"],
+    //     'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+    //     'charset'  => 'utf8',
+    //     'prefix'   => '',
+    //     'schema'   => 'public',
+    // ],
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +70,28 @@ return [
             'prefix'   => '',
         ],
 
-        'mysql' => [
+        // 'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST', 'localhost'),
+        //     'database'  => env('DB_DATABASE', 'forge'),
+        //     'username'  => env('DB_USERNAME', 'forge'),
+        //     'password'  => env('DB_PASSWORD', ''),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
+
+        'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-            'strict'    => false,
-        ],
+        ),
 
         'pgsql' => [
             'driver'   => 'pgsql',
