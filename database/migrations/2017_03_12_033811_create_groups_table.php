1f7b48ce2e2b1b11a6dcb3ab79b515806143a360
quy <dinhquyf8@gmail.com>
2017-03-22 00:15:07 +0700

    fix UserController

--- database/migrations/2017_03_12_033811_create_groups_table.php
+++ database/migrations/2017_03_12_033811_create_groups_table.php
@@ -13,7 +13,7 @@ class CreateGroupsTable extends Migration
     public function up()
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->increments('id');
+            $table->primary('id');
             $table->string('name');
             $table->string('description');
             $table->timestamps();
