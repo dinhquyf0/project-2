891325cd4e54aa1b54de617c810ebbdaa54cc589
quy <dinhquyf8@gmail.com>
2017-03-22 15:12:45 +0700

    Add Heroku creds and update default route to hit the DB.

---
+++ database/migrations/2017_03_22_074331_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userid')->unsigned();
+            $table->string('comment');
+            $table->integer('place', false, true);
+            $table->foreign('topic_1')->references('id')->on('topics')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
