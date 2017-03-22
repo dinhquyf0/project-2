399ac0892074ae22616e2636040fe39cbbfe8007
quy <dinhquyf8@gmail.com>
2017-03-22 15:02:07 +0700

    Add Heroku creds and update default route to hit the DB.

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->length(50)->unique();
+            $table->string('password')->length(60);
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->length(50)->unique();
+            $table->integer('group_id')->unsigned();
+            $table->integer('phonenumber');
+            $table->date('dateofbirth');
+            $table->tinyInteger('lock');
+            $table->tinyInteger('status');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
